8ed2c656d3f5487b64e0cae9d14c2dfac959555f
IPMaddBuddha <37713898+IanPriceMB@users.noreply.github.com>
2018-04-30 16:51:33 -0600

    last commit

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ var monIndex = 0;
 
 function setup(){
 //make our weapons (buttons)
-    for(var i = 0; i < 4; i++) {
+    for(var i = 0; i < weaponImages; i++) {
         weapon = $("<div>");
         weapon.addClass("wepButton");
         weapon.attr('id', "btn" + i);
@@ -24,7 +24,7 @@ function setup(){
     for(var i = 0; i < 4; i++) {
         var elem = document.createElement("img");
         document.getElementById("btn" + i).appendChild(elem);
-    //will cycle trhough pics in array (for attaching to buttons) when called in for loop after buttons are made
+//will cycle trhough pics in array (for attaching to buttons) when called in for loop after buttons are made
         function weaponPic(){
                 var a = weaponImages[imgIndex];
                 imgIndex++;
@@ -36,7 +36,6 @@ function setup(){
     HP = Math.floor(Math.random() * 100) +40;
     document.getElementById("monsterhp").innerHTML = HP;
 }
-
 //will cycle trhough pics in array (for creating monsters)
 function momo(){
     var elem = document.createElement("img");
@@ -48,8 +47,6 @@ function momo(){
             }
     elem.src = monsterPic();
 }
-
-
 //new monster button
 $("body").on("click", "#new", function(){
     $( ".weapons" ).empty();
@@ -61,13 +58,16 @@ $("body").on("click", "#new", function(){
         monIndex=0
     }
 })
-
 // doing doing hp damage with our buttons & win conditions?
 $("body").on("click", ".wepButton", function(){
-    if((HP- $(this).attr("power")) == 0){
+    if((HP- $(this).attr("power")) == 0 && monIndex === 2){
+        $(".weapons").empty();
+        $("#monsterhp").text("0");
+        alert("You won! Press 'Summon a New Monster' to restart.");
+    } else if((HP- $(this).attr("power")) == 0){
         $( ".weapons" ).empty();
         $("#monsterhp").text("0");
-        alert("You won! Spawn the next monster");
+        alert("You won! Spawn the next monster.");
     } else if ((HP- $(this).attr("power")) < 0){
         alert("You lose. Try again.");
         $( ".weapons" ).empty();
@@ -78,22 +78,4 @@ $("body").on("click", ".wepButton", function(){
         $("#monsterhp").text(HP);
     }
 })
-
-//disable buttons if game end
-if(HP == 0 || HP < 0){
-    $( ".weapons" ).empty();
-    $("#monsterhp").text("0");
-}
-// win lose conditions
-$("body").on("click", ".wepButton", function(){
-    if (HP === 0) {
-        alert("Spawn the next monster");
-    }
-    else if (HP < 0) {
-        alert("You lose. Try again.");
-    }
-
-
-})
-
 });
